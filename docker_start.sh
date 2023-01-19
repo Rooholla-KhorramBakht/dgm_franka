779bd589f4f11d0eb5a95ed997deaf42d77b7cf0
Rooholla-Khorrambakht <learn.kho@gmail.com>
2023-01-18 19:07:13 -0500

    mounting point is fixed

--- docker_start.sh
+++ docker_start.sh
@@ -1,1 +1,1 @@
-sudo docker run -it --network host --privileged --volume "$(pwd)":/root/dgm_franka robocaster/dgm-franka:amd64
+sudo docker run -it --network host --privileged --volume "$(pwd)":/root/host_dgm_franka robocaster/dgm-franka:amd64
